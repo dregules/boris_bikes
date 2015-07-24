a204aa10737d3478e6e5fe5b2ade9ee2cc15921f
Diego Regules <diegoregules@gmail.com>
2015-07-24 11:46:02 +0100

    Added working and broken methods

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,4 +4,21 @@ describe Bike do
   it "Responds to working" do
     expect(subject).to respond_to :working?
   end
+
+  it " Responds to #report_broken" do
+    expect(subject).to respond_to :report_broken
+  end
+
+  it " Responds to #broken?" do
+    bike = Bike.new
+    expect(bike).to respond_to :broken?
+
+  end
+
+  it " gets broken when reported as broken" do
+    bike = Bike.new
+    bike.report_broken
+    expect(bike).to be_broken
+
+  end
 end
